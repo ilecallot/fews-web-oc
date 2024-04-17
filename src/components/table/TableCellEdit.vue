ecf6c0f986cb0654db56063c76df65c71d52e6fe
hvangeffen <hunter.vangeffen@vortech.nl>
2024-04-17 16:22:09 +0200

    Fix cell edit showing comma decimal seperator in edge

--- src/components/table/TableCellEdit.vue
+++ src/components/table/TableCellEdit.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="table-cell-editable">
     <input type="checkbox" @change="(event) => selectItem(event)" />
+    <!-- Edge does not respect lang="en-US" as such to not have ',' as decimal separator we need type="text" -->
     <input
       :ref="`${props.item.date}-${props.id}-value`"
       v-model.number="currentItem.y"
       class="table-cell-edit"
-      type="number"
+      type="text"
+      inputmode="numeric"
       placeholder="value"
       @change="(event) => editValue(event, currentItem.y)"
     />
